25f64f1ac61fedb488974886a5aead3d61e1f757
Ednario Andrade <contatoednario@gmail.com>
2025-05-14 10:36:03 -0300

    fix: resolve footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -119,10 +119,10 @@ export default function Home() {
       <footer id="contato" className="bg-blue-900 dark:bg-gray-800 text-white py-6 px-4 text-sm">
         <div className="container mx-auto text-center space-y-1">
             <p className="mb-2">Horário:</p>
-            <p>Segunda a Quinta</p>
+            <p>Segunda a Quinta:</p>
             <p>Manhã: Segunda a Quinta das 07:30h às 11:30h</p>
             <p>Tarde: 13:30h às 17:00h</p>
-            <p>Sexta das 08:00h às 12:00h</p>
+            <p>Sexta: das 08:00h às 12:00h</p>
         </div>
         <hr className="h-px my-8 bg-gray-400 border-0 dark:bg-gray-700"/>
         <div className="container mx-auto text-center space-y-1">
