b6643791e8375721b435a2b92058e488c9eac305
Ednario Andrade <contatoednario@gmail.com>
2025-05-14 10:34:43 -0300

    fix: resolve footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -116,14 +116,19 @@ export default function Home() {
           </div>
         </div>
       </section>
-
       <footer id="contato" className="bg-blue-900 dark:bg-gray-800 text-white py-6 px-4 text-sm">
         <div className="container mx-auto text-center space-y-1">
-          <p>Prefeito Moesio Loiola de Melo</p>
-          <p>CNPJ: 07.416.704/0001-99</p>
-          <p>Contato: (88) 3533-1666 | gabinete@campossales.ce.gov.br</p>
-          <p>Endereço: Travessa Sul, 440 - Centro, 63.150-000</p>
-          <p>Horário: Segunda a Sexta das 08:00h às 12:00h</p>
+            <p className="mb-2">Horário:</p>
+            <p>Segunda a Quinta</p>
+            <p>Manhã: Segunda a Quinta das 07:30h às 11:30h</p>
+            <p>Tarde: 13:30h às 17:00h</p>
+            <p>Sexta das 08:00h às 12:00h</p>
+        </div>
+        <hr className="h-px my-8 bg-gray-400 border-0 dark:bg-gray-700"/>
+        <div className="container mx-auto text-center space-y-1">
+          <p>Secretaria Morgana Kelly</p>
+          <p>Contato: saudecampossalesce@gmail.com</p>
+          <p>Endereço: Bárbara Pereira de Alencar, 652 - Centro, 63.150-000</p>
         </div>
       </footer>
     </>
